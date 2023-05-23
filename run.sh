c5d3891e9e3d9b20117f9508c420c7cb771275b6
Santiago <srivas@itba.edu.ar>
2023-05-23 18:03:17 -0300

    GDB debbuging added

--- run.sh
+++ run.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+
+if [[ "$1" = "gdb" ]]; then
+
+ qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512 -d int
+
+else
+
+ qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512
+
+fi
+
+
 # qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=snd0 -device ich9-intel-hda  -device hda-duplex,audiodev=snd0
-qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 
+# qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512
